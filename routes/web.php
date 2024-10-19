bb13a177b20b9915c3d2200a5cc507716a72b67f
HaikalAkasyah <sarhesa726@gmail.com>
2024-10-19 12:02:11 +0700

    laravel 4

--- routes/web.php
+++ routes/web.php
@@ -1,53 +1,61 @@
 <?php
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\loginregistercontroller;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginRegisterController;
+use App\Http\Controllers\AdminController;
 
-
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('home');
 });
 
-Route::get('/auth/login', [loginregistercontroller::class, 'login'])->name('auth.login');
+Route::get('/welcome', function () {
+    return view('welcome');
+});
 
-Route::get('/auth/register', [loginregistercontroller::class, 'register'])->name('auth.register');
-
+Route::get('/login', [LoginRegisterController::class, 'login'])->name('auth.login');
+Route::get('/register', [LoginRegisterController::class, 'register'])->name('auth.register');
 Route::get('/user/home', [LoginRegisterController::class, 'userHome'])->name('user.home');
-
 Route::get('/admin/home', [LoginRegisterController::class, 'adminHome'])->name('admin.home');
-
-Route::post('/postRegister', [LoginRegisterController::class,'postRegister'])->name('postRegister');
-
-Route::post('/postLogin', [LoginRegisterController::class, 'postLogin'])->name('postLogin');
-
-Route::get('/logout', [LoginRegisterController::class, 'logout'])->name('logout');
 
 Route::middleware(['guest'])->group(function () {
     Route::get('/', function () {
-    return view('home');
+        return view('home');
     });
+    Route::get('/auth/login', [LoginRegisterController::class, 'login'])->name('auth.login');
+    Route::get('/auth/register', [LoginRegisterController::class, 'register'])->name('auth.register');
+});
 
-Route::get('/auth/login', [LoginRegisterController::class, 'login'])
-->name('auth.login');
+Route::group(['middleware' => ['auth', 'checklevel:admin']], function () {
+    Route::get('/admin/home', [LoginRegisterController::class, 'adminHome'])->name('admin.home');
+    Route::get('/admin/tambah', [AdminController::class, 'tambah'])->name('admin.tambah');
+    Route::get('/editAdmin/{id}', [AdminController::class, 'editAdmin'])->name('editAdmin');
+    Route::get('/deleteAdmin/{id}', [AdminController::class, 'deleteAdmin'])->name('deleteAdmin');
 
-Route::get('/auth/register', [LoginRegisterController::class,
-    'register'])->name('auth.register');
-    });
+    Route::get('/admin/buku', [AdminController::class, 'adminBuku'])->name('admin.buku');
+    Route::get('/admin/tambahBuku', [AdminController::class, 'tambahBuku'])->name('admin.tambahBuku');
+    Route::get('/admin/editBuku/{id}', [AdminController::class, 'editBuku'])->name('admin.editBuku');
+    Route::get('/admin/deleteBuku/{id}', [AdminController::class, 'deleteBuku'])->name('admin.deleteBuku');
 
-Route::group(['middleware' => ['auth', 'checklevel:admin']], function ()
-    {
-Route::get('/admin/home', [LoginRegisterController::class,
-    'adminHome'])->name('admin.home');
-    });
-    Route::group(['middleware' => ['auth', 'checklevel:user']], function () {
-    Route::get('/user/home', [LoginRegisterController::class,
-    'userHome'])->name('user.home');
-    });
+    Route::get('/admin/peminjaman', [AdminController::class, 'adminPeminjaman'])->name('admin.peminjaman');
+    Route::get('/admin/tambahPeminjaman', [AdminController::class, 'tambahPeminjaman'])->name('admin.tambahPeminjaman');
+    Route::get('/admin/editPeminjaman/{id}', [AdminController::class, 'editPeminjaman'])->name('admin.editPeminjaman');
+    Route::get('/admin/deletePeminjaman/{id}', [AdminController::class, 'deletePeminjaman'])->name('admin.deletePeminjaman');
+    Route::get('/admin/detailPeminjaman/{id_peminjaman}/{id_user}/{id_buku}', [AdminController::class, 'detailPeminjaman'])->name('admin.detailPeminjaman');
+    Route::get('/admin/cetakPeminjaman', [AdminController::class, 'cetakDataPeminjaman'])->name('admin.cetakDataPeminjaman');
+});
 
-Route::get('/logout', [LoginRegisterController::class, 'logout'])
-->name('logout');
+Route::post('/postTambahBuku', [AdminController::class, 'postTambahBuku'])->name('postTambahBuku');
+Route::post('/postEditBuku/{id}', [AdminController::class, 'postEditBuku'])->name('postEditBuku');
+Route::post('/postTambahPeminjaman', [AdminController::class, 'postTambahPeminjaman'])->name('postTambahPeminjaman');
+Route::post('/postEditPeminjaman/{id}', [AdminController::class, 'postEditPeminjaman'])->name('postEditPeminjaman');
 
-Route::post('/postLogin', [LoginRegisterController::class, 'postLogin'])
-->name('postLogin');
-    Route::post('/postRegister', [LoginRegisterController::class,
-    'postRegister'])->name('postRegister');
+Route::post('/tambahAdmin', [AdminController::class, 'postTambahAdmin'])->name('postTambahAdmin');
+Route::post('/postEditAdmin/{id}', [AdminController::class, 'postEditAdmin'])->name('postEditAdmin');
+
+Route::group(['middleware' => ['auth', 'checklevel:user']], function () {
+    Route::get('/user/home', [LoginRegisterController::class, 'userHome'])->name('user.home');
+});
+
+Route::post('/postRegister', [LoginRegisterController::class, 'postRegister'])->name('postRegister');
+Route::post('/postLogin', [LoginRegisterController::class, 'postLogin'])->name('postLogin');
+Route::get('/logout', [LoginRegisterController::class, 'logout'])->name('logout');
